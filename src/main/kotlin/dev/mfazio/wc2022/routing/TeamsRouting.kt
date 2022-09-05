a799434639701d47807fd8ff13789626263b553f
Michael Fazio <mfazio23@gmail.com>
2022-09-05 12:24:25 -0500

    Whoops, broke Teams routing.

--- src/main/kotlin/dev/mfazio/wc2022/routing/TeamsRouting.kt
+++ src/main/kotlin/dev/mfazio/wc2022/routing/TeamsRouting.kt
@@ -1,8 +1,6 @@
 package dev.mfazio.wc2022.routing
 
-class TeamsRouting {
 import dev.mfazio.wc2022.types.api.TeamApiModel
-import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
