d9dda3c1d5f75be6f9674cb02f96fc9b4f022df8
Michael Fazio <mfazio23@gmail.com>
2022-11-12 08:20:49 -0600

    Rankings can be optional

--- src/main/kotlin/dev/mfazio/wc2022/types/domain/Ranking.kt
+++ src/main/kotlin/dev/mfazio/wc2022/types/domain/Ranking.kt
@@ -1,7 +1,7 @@
 package dev.mfazio.wc2022.types.domain
 
 data class Ranking(
-    val ranking: Int,
+    val ranking: Int?,
     val previousRanking: Int? = null,
     val points: Double? = null,
 )
