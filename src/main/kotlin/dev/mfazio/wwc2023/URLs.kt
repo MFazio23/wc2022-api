e58640c39cceafbb01bc712f52df2a38e1a30e79
Michael Fazio <mfazio23@gmail.com>
2023-07-30 13:56:42 -0500

    Start some standings work

--- src/main/kotlin/dev/mfazio/wwc2023/URLs.kt
+++ src/main/kotlin/dev/mfazio/wwc2023/URLs.kt
@@ -3,12 +3,17 @@ package dev.mfazio.wwc2023
 import de.sharpmind.ktor.EnvConfig
 
 object URLs {
-    const val fifaRankingsUrl = "https://www.fifa.com/api/ranking-overview?locale=en&dateId=ranking_20230609"
     const val competitionId = 103
+    const val seasonId = 285026
+    const val stageId = 285033
+
+    const val fifaRankingsUrl = "https://www.fifa.com/api/ranking-overview?locale=en&dateId=ranking_20230609"
     const val scheduleUrl =
         "https://api.fifa.com/api/v3/calendar/matches?from={startDateTime}&to={endDateTime}&language=en&count=4&idCompetition=$competitionId"
     const val matchDetailsUrl =
         "https://api.fifa.com/api/v3/live/football/$competitionId/255711/{stageId}/{matchId}?language=en"
+    const val standingsUrl =
+        "https://api.fifa.com/api/v3/calendar/$competitionId/$seasonId/$stageId/standing?language=en"
 
     const val firebaseRankingsUrl = "rankings"
     const val firebaseScheduleUrl = "schedule"
---
+++ src/main/kotlin/dev/mfazio/wwc2023/services/StandingsService.kt
@@ -0,0 +1,7 @@
+package dev.mfazio.wwc2023.services
+
+object StandingsService : ApiService() {
+    suspend fun saveStandingsToFirebase(standings: ) {
+
+    }
+}
